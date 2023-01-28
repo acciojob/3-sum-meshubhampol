5331c09e343baa3a18a18694095a24e31846c7df
meshubhampol <116621224+meshubhampol@users.noreply.github.com>
2023-01-28 11:37:01 +0530

    minor change

--- script.js
+++ script.js
@@ -9,9 +9,10 @@ function threeSum(arr, target) {
 
     while(j<k) {
       let sum2=arr[j]+arr[k];
-      if( ((sum1+sum2) - target) < ans-target) {
+      if( (Math.abs(target-(sum1+sum2))) < Math.abs(target-ans) ) {
         ans=sum1+sum2;
       }
+      
       if((sum1+sum2)=== target) {
         return sum1+sum2;
       }
