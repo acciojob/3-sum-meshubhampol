c7add2efc42c0329a3332ef45bfd63381d147972
meshubhampol <116621224+meshubhampol@users.noreply.github.com>
2023-01-29 16:48:07 +0530

    handled edge case

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ function threeSum(arr, target) {
   //your code here
   let ans=Number.MAX_VALUE;
   arr.sort();
+  if(arr.length<3) {
+    return 0;
+  }
   for(let i=0;i<arr.length;i++) {
     let sum1=arr[i];
     let j=i+1;
@@ -9,14 +12,15 @@ function threeSum(arr, target) {
 
     while(j<k) {
       let sum2=arr[j]+arr[k];
-      if( (Math.abs(target-(sum1+sum2))) < (target-ans) ) {
+
+      if( (Math.abs(target-(sum1+sum2)) ) < Math.abs(target-ans) ) {
         ans=sum1+sum2;
       }
 
-      if((sum1+sum2)=== target) {
+      if((sum1+sum2) === target) {
         return sum1+sum2;
       }
-      else if((sum1+sum2)>target) {
+      else if((sum1+sum2) > target) {
         k--;
       }
       else {
